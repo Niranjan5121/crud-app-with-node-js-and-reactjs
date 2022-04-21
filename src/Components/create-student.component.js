d98782c3278934ed0aba8b6d042624b44f1f19f8
Niranjan Singh <NiranjanS@WinjitTechnologiesPvtLtd.local>
2022-04-21 16:56:06 +0530

    initial commit

---
+++ src/Components/create-student.component.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import StudentForm from "./StudentForm";
+
+const CreateStudent = () => {
+  const [formValues, setFromValue] = useState({
+    name: "",
+    email: "",
+    rollno: "",
+  });
+
+  const onSubmit = (studentObject) => {
+    axios
+      .post("http://localhost:4000/students/create-student", studentObject)
+      .then((res) => {
+        if (res.status === 200) alert("Student successfully created");
+        else Promise.reject();
+      })
+      .catch((err) => alert("Something went wrong"));
+  };
+
+  return (
+    <StudentForm
+      initialValues={formValues}
+      onSubmit={onSubmit}
+      enableReinitialize
+    >
+      Create Student
+    </StudentForm>
+  );
+};
+
+export default CreateStudent;
